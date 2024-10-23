ff92a2af91f104fbf293dc2d1f937ee127f5d439
Chinex <astauchiha234@gmail.com>
2024-10-23 01:30:09 +0100

    recent

--- src/components/MainNavagetion.jsx
+++ src/components/MainNavagetion.jsx
@@ -35,7 +35,7 @@ const MainNavagetion = () => {
 					</div>
 					<ul
 						tabIndex={0}
-						className='menu menu-sm dropdown-content bg-stone-900 rounded-box z-[1] mt-3 w-52 p-2 shadow'>
+						className='menu menu-sm dropdown-content bg-[#111111] rounded-box z-[1] mt-3 w-52 p-2 shadow'>
 						{nav.map((item, index) => (
 							<li key={index}>
 								<NavLink
